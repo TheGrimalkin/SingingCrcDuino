c9ea777e37463dbcae3252b08f3557cbb2d2babf
TheGrimalkin <118630535+TheGrimalkin@users.noreply.github.com>
2024-11-05 22:29:15 -0500

    Part one of fixing bugs

--- Main.py
+++ Main.py
@@ -13,6 +13,7 @@ class TRACK:
     name = "UNDEFINED"
     notes = []
     key = "C or UNDEFINED"
+    starting_time = 0
 
 midi_file = MidiFile()
 name_of_file = "UNDEFINED"
@@ -55,37 +56,48 @@ tracks_list = []
 
 # Iterate through all the tracks
 for track in midi_file.tracks:
+    #print(track)
     newTRACK = TRACK()
-    tracks_list.append(newTRACK)
     previousNOTE = NOTE()       #to add the EndSilence
-    for msg in midi_file:
+    for msg in track:
         # Filter different msg types
         if msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0): #0 velocity = stop sound
-            previousNOTE.time = round(msg.time*1000) ##convert in ms
+            previousNOTE.time = msg.time
+            #previousNOTE = NOTE()
         elif msg.type == 'note_on':
-                newNOTE = NOTE()
-                newNOTE.noteType = NoteType[msg.note]
-                newTRACK.notes.append(newNOTE)
-                previousNOTE.endSilence = round(msg.time*1000)
-                previousNOTE = newNOTE
+            if len(newTRACK.notes) == 0:
+                newTRACK.starting_time = msg.time
+            newNOTE = NOTE()
+            newNOTE.noteType = NoteType[msg.note]
+            previousNOTE.endSilence = msg.time
+            previousNOTE = newNOTE  #Est-ce que c'est un pointer ?
+            newTRACK.notes.append(newNOTE)
         #elif msg.type == 'set_tempo':
             #tempo = msg.tempo
         elif msg.type == 'track_name':
             newTRACK.name = msg.name
+    tracks_list.append(newTRACK)
 
 new_song_file = open("Songs/Testt.txt", mode='w')
 new_song_file.write('\n'+ '**** New Song ****' + '\n\n' )
 i = 0
-for TRACK in tracks_list:
-    new_song_file.write('Track number ' + str(i) + ' : ' + tracks_list[i].name + '\n')
+for _track in tracks_list:
+    new_song_file.write('Track number ' + str(i) + ' : ' + _track.name + '\n')
     new_song_file.write('Note notes[] = { ')
-    for note in TRACK.notes:
-        new_song_file.write('{' + str(note.time) + ',' + note.noteType + '},')
-        if note.endSilence > 0:
-            new_song_file.write('{' + str(note.endSilence) + ',NOTE_SILENCE},')
+    if TRACK.starting_time != 0:
+        new_song_file.write('{'+str(_track.starting_time) + ',NOTE_SILENCE},')
+    for _note in _track.notes:
+        new_song_file.write('{' + str(_note.time) + ',' + _note.noteType + '},')
+        if _note.endSilence > 0:
+            new_song_file.write('{' + str(_note.endSilence) + ',NOTE_SILENCE},')
 
     new_song_file.write(' Note::END };' +'\n')
     i+=1
 
 midi_file.print_tracks()
-#print_notes()
+
+"""for TRACK in tracks_list:
+    print('NEWTrACK')
+    for note in TRACK.notes:
+        print(note.noteType)
+#print_notes()"""
